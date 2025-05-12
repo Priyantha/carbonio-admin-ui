a1c5d95e3f8712248704ca7da26e146c4ed707ab
Priyantha Bleeker <priyantha.bleeker@gmail.com>
2025-05-12 09:58:48 +0200

    feat: add grantRightToAdmin to assign rights using zmprov grr

--- src/delegated-admin/cliWrapper.js
+++ src/delegated-admin/cliWrapper.js
@@ -5,3 +5,11 @@ export async function revokeRightFromAdmin(adminEmail, targetType, targetName, r
   const cmd = `zmprov rra ${target} zimbra:${right} ${opt} ${adminEmail}`;
   await exec(cmd);
 }
+
+export async function grantRightToAdmin(adminEmail, targetType, targetName, right, attr) {
+  const { exec } = await import('child_process');
+  const target = `${targetType} -n ${targetName}`;
+  const opt = attr ? `${attr} ` : "";
+  const cmd = `zmprov grr ${target} zimbra:${right} ${opt} ${adminEmail}`;
+  await exec(cmd);
+}
