75e12084dfcb6996340e522ff52222c4a89d2bd3
Priyantha Bleeker <priyantha.bleeker@gmail.com>
2025-05-12 07:58:10 +0200

    feat: add deleteDelegatedAdmin SOAP function

--- src/delegated-admin/soapClient.js
+++ src/delegated-admin/soapClient.js
@@ -1,9 +1,9 @@
 export async function createDelegatedAdmin(email, password, domain) {
   const soapBody = `
-    <soap:Envelope xmlns:psoap="http://www.w3.org/2003/05/soap-envelope">
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
       <soap:Header>
         <context xmlns="urn:zimbra">
-          <authToken xmlns="urn:zimbra"></soap:Header>
+          <authToken xmlns="urn:zimbra"/>
         </context>
       </soap:Header>
       <soap:Body>
@@ -14,21 +14,36 @@ export async function createDelegatedAdmin(email, password, domain) {
         </CreateAccountRequest>
       </soap:Body>
     </soap:Envelope>
-  `; 
+  `;
   const res = await fetch("/service/admin/soap", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/soap+xml"
-    },
+    headers: { "Content-Type": "application/soap+xml" },
     body: soapBody
   });
-
-  if (!res.ok) {
-    throw new Error(`FAILED: ${res.status} - ${rest.statusText}`);
-  }
-
+  if (!res.ok) throw new Error(`http://failed: ${res.status}`);
   const text = await res.text();
-  // TODO: Parse XML and check success
-
   return text;
+}
+
+export async function deleteDelegatedAdmin(email) {
+  const soapBody = `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+  <soap:Header>
+    <context xmlns="urn:zimbra">
+      <authToken xmlns="urn:zimbra"/>
+    </context>
+  </soap:Header>
+  <soap:Body>
+    <DeleteAccountRequest xmlns="urn:zimbraAdmin">
+      <account name="${email}" />
+    </DeleteAccountRequest>
+  </soap:Body>
+ </soap:Envelope>`;
+
+  const res = await fetch("/service/admin/soap", {
+    method: "POST",
+    headers: { "Content-Type": "application/soap+xml" },
+    body: soapBody
+  });
+  if (!res.ok) throw new Error(`http://failed: ${res.status}`);
+  return await res.text();
 }
